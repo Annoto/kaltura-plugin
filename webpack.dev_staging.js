dcfbf2671220c573abbbf8362fe4d4ca3d050153
Genadi Sokolov <genadi@annoto.net>
2018-11-04 01:32:21 +0200

    (dev) start-https and start-staging

---
+++ webpack.dev_staging.js
@@ -0,0 +1,20 @@
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+/* eslint func-names: ["error", "never"] */
+
+const path = require('path');
+const Merge = require('webpack-merge');
+const CommonConfig = require('./webpack.common');
+
+module.exports = function (env) {
+    const commonEnv = env;
+    commonEnv.appURL = `https://staging-app.annoto.net`;
+    return Merge(CommonConfig(commonEnv), {
+        devtool: 'cheap-module-inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'dist'),
+            port: 9001,
+        },
+        plugins: [
+        ],
+    });
+};
